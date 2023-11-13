bf1d8daf03bd193005255b78436da57e9a2f8e94
ardu1na <arduinadelbosque@gmail.com>
2023-11-13 15:17:06 -0300

    connect FE

--- core/urls.py
+++ core/urls.py
@@ -4,8 +4,16 @@ from django.urls import path, include
 from django.conf.urls.static import static
 from core import settings
 
+
+from django.shortcuts import render
+
+def test(request):
+    return render (request,'page/index.html',{})
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/prensa/', include('prensa.urls')),
+    
+    path('', test)
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
