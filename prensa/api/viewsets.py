f56cacf27a50c8f9c91a92268157b43c804a4941
ardu1na <arduinadelbosque@gmail.com>
2023-11-21 12:15:53 -0300

    endpoint only get method allow

--- prensa/api/viewsets.py
+++ prensa/api/viewsets.py
@@ -1,11 +1,16 @@
-
-from rest_framework import viewsets
-from prensa.api.serializers import ArticuloListSerializer,  ArticuloDetailSerializer
+from rest_framework import viewsets, permissions
+from prensa.api.serializers import ArticuloListSerializer, ArticuloDetailSerializer
 from prensa.models import Articulo
 
 class ArticulosViewSet(viewsets.ModelViewSet):
     queryset = Articulo.objects.filter(publicado=True)
+
     def get_serializer_class(self):
         if self.action == 'list':
             return ArticuloListSerializer
         return ArticuloDetailSerializer
+
+    def get_permissions(self):
+        if self.action in ['list', 'retrieve']:
+            return [permissions.AllowAny()]
+        return [permissions.IsAuthenticated()]
