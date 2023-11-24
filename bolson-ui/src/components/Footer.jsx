00bffc25bc4903eb25dc30e9e410530154da4de5
ardu1na <arduinadelbosque@gmail.com>
2023-11-24 11:07:08 -0300

    footer in react

---
+++ bolson-ui/src/components/Footer.jsx
@@ -0,0 +1,37 @@
+import logo_footer from "../logo_escudo.png";
+
+
+export const Footer = () => {
+    return (
+        <>
+        <footer className="justify-content-around mx-0 bg-dark d-flex p-4 footer fixed-bottom">
+            <div className="row pt-3">
+
+                <div className="col mt-4 d-none d-sm-block">
+                    <img src={logo_footer} alt="Logo Municipalidad El Bolsón" width="120" />
+                </div>
+
+                <div className="col text-white my-4 mx-5 ">
+                    <h5><span>TELÉFONOS DE EMERGENCIA</span></h5>
+                    <ul className="nav flex-column">
+                        <li className="nav-item mb-2 text-secondary"><strong>911</strong> | Emergencias</li>
+                        <li className="nav-item mb-2 text-secondary"><strong>100</strong> | Bomberos</li>
+                        <li className="nav-item mb-2 text-secondary"><strong>103</strong> | Incendios forestales</li>
+                        <li className="nav-item mb-2 text-secondary"><strong>107</strong> | Hospital</li>
+                        <li className="nav-item mb-2 text-secondary"><strong>135</strong> | Gendarmería Nacional</li>
+                    </ul>
+                </div>
+
+                <div className="col text-white my-5 mx-5">
+                    <h5>SEGUINOS EN LAS REDES</h5>
+                        <a href="https://www.facebook.com/MunicipalidadElBolson" className="text-secondary fs-4 px-3" title="Facebook" aria-label="Facebook"><i className="bi bi-facebook"></i></a>
+                        <a href="https://www.instagram.com/municipalidadelbolson/" className="text-secondary fs-4 px-3" title="Instagram" aria-label="Instagram"><i className="bi bi-instagram"></i></a>
+                </div>
+            </div>
+        </footer>
+
+        </>
+  )
+};
+
+
--- bolson-ui/src/pages/Layout.jsx
+++ bolson-ui/src/pages/Layout.jsx
@@ -1,5 +1,6 @@
 import { Outlet } from "react-router-dom";
 import { Header } from "../components/Header";
+import { Footer } from "../components/Footer";
 
 
 export const Layout = () => {
@@ -9,6 +10,7 @@ export const Layout = () => {
 
     <Outlet />
    
+    <Footer />
 
     </>
   )
