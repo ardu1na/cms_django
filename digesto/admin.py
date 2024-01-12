defc31c5020b49853bb03cbeb12c942bf740db22
ardu1na <arduinadelbosque@gmail.com>
2024-01-12 12:15:00 -0300

    starting digesto backend

--- digesto/admin.py
+++ digesto/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import ItemDigesto, Tema
+
+admin.site.register(ItemDigesto)
+admin.site.register(Tema)
--- digesto/models.py
+++ digesto/models.py
@@ -1,3 +1,124 @@
 from django.db import models
 
-# Create your models here.
+
+
+class Tema(models.Model):
+    nombre = models.CharField(
+                            max_length=200
+            )
+    
+    
+    def __str__ (self):
+        return self.nombre
+
+
+class ItemDigesto (models.Model):
+    ORDENANZA = 'ordenanza'
+    RESOLUCION = 'resolución'
+    COMUNICADO = 'comunicado'
+    DECLARACION = 'declaración de interés'
+
+    CATEGORIA_CHOICES = (
+        (ORDENANZA, ('ordenanza')),
+        (RESOLUCION, ('resolución')),
+        (COMUNICADO, ('modificada')),
+        (DECLARACION, ('comunicado de interés')),
+        
+    )
+
+    EN_VIGENCIA = 'en vigencia'
+    DEROGADA = 'derogada'
+    MODIFICADA = 'modificada'
+    
+    ESTADO_CHOICES = (
+        (EN_VIGENCIA, ('en vigencia')),
+        (DEROGADA, ('derogada')),
+        (MODIFICADA, ('modificada')),
+    )
+
+    GENERAL = 'general'
+    PARTICULAR = 'particular'
+    
+    ALCANCE_CHOICES = (
+        (GENERAL, ('general')),
+        (PARTICULAR, ('particular')),
+    )
+
+
+
+    categoria = models.CharField(
+                        max_length=50,
+                        choices=CATEGORIA_CHOICES,
+                        )
+    
+    fecha = models.DateField(
+                        )
+    
+    titulo = models.CharField(
+                        max_length=200,
+                        )
+    
+    numero = models.CharField(
+                        max_length=50,
+                        null=True,
+                        blank=True
+                        )
+    
+    tema = models.ManyToManyField(
+                        Tema,
+                        related_name="ordenanzas",
+                        null=True,
+                        blank=True
+                        )
+    estado = models.CharField(
+                        max_length=50,
+                        choices=ESTADO_CHOICES,
+                        null=True,
+                        blank=True
+                        )
+    alcance = models.CharField(
+                        max_length=50,
+                        choices=ALCANCE_CHOICES,
+                        null=True,
+                        blank=True
+                        )
+    observaciones = models.TextField(
+                        null=True,
+                        blank=True
+                        )
+    resumen = models.TextField(
+                        null=True,
+                        blank=True
+                        )
+    modifica_a = models.ManyToManyField(
+                        'ItemDigesto',
+                        related_name="modificada_por",
+                        null=True,
+                        blank=True
+                        )
+    cita_a = models.ManyToManyField(
+                        'ItemDigesto',
+                        related_name="citada_por",
+                        null=True,
+                        blank=True
+                        )
+    texto = models.TextField(
+                        null= True,
+                        blank = True,
+                        )
+    archivo = models.FileField(
+                        null=True,
+                        blank=True,
+                        upload_to='digesto'
+    )
+    
+    @property
+    def anio(self):
+        return self.fecha.year
+
+    def __str__ (self):
+        return f'{self.categoria} n° {self.numero} - {self.anio}  {self.titulo}'
+    
+    class Meta:
+        verbose_name = "Item del Digesto Municipal"
+        verbose_name_plural = "Items del Digesto Municipal"
