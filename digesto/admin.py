448194e298014a2745a3530f8cf30073d80b6608
ardu1na <arduinadelbosque@gmail.com>
2024-01-25 13:40:06 -0300

    changin display in itemdigesto admin

--- digesto/admin.py
+++ digesto/admin.py
@@ -5,18 +5,23 @@ from digesto.models import ItemDigesto, Tema, Archivo
 from digesto.resources import ItemDigestoResource, TemaResource, ArchivoResource
 
 
-
 class ItemDigestoAdmin(ImportExportModelAdmin):
-    list_display = ['categoria', 'numero',  'fecha', 'alcance', 'display_temas']
-    search_fields = ['categoria','numero', 'observaciones', 'texto', 'temas__nombre']
+    list_display = ['custom_categoria_numero', 'fecha', 'alcance', 'display_temas']
+    search_fields = ['categoria', 'numero', 'observaciones', 'texto', 'temas__nombre']
     list_filter = ['categoria', 'estado', 'alcance', 'temas']
-    list_display_links = ['numero', 'categoria', 'fecha', 'alcance']
+    list_display_links = ['custom_categoria_numero', 'fecha', 'alcance']
     resource_class = ItemDigestoResource
+
+    def custom_categoria_numero(self, obj):
+        return f'{obj.categoria.capitalize()} {obj.numero}'
+    custom_categoria_numero.short_description = 'Objeto'
 
     def display_temas(self, obj):
         return ', '.join([tema.nombre for tema in obj.temas.all()])
     display_temas.short_description = 'Temas'
+
 admin.site.register(ItemDigesto, ItemDigestoAdmin)
+
 
 
 
