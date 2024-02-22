bac9b21a163502179d60e0c18e6fa07929c43e90
ardu1na <arduinadelbosque@gmail.com>
2024-02-22 11:12:36 -0300

    cultura carrousel fe endpoint conn

--- cultura/api/serializers.py
+++ cultura/api/serializers.py
@@ -2,14 +2,9 @@ from datetime import datetime
 from rest_framework import serializers
 from cultura.models import Evento
 
-def get_formatted_date(fecha):
-    return datetime.strftime(fecha, "%d/%m/%Y %H:%M hs.")
-
 class LastEventosListSerializer(serializers.ModelSerializer):
     class Meta:
         model = Evento
-        fields = ['id', 'titulo', 'descripcion', 'imagen', 'date']
+        fields = ['id', 'titulo', 'descripcion', 'imagen', 'fecha']
     
-    def get_date(self, obj):
-        return get_formatted_date(obj.fecha)
     
--- cultura/api/viewsets.py
+++ cultura/api/viewsets.py
@@ -1,18 +1,12 @@
-from rest_framework import viewsets, permissions, pagination
+from rest_framework import viewsets, permissions
 
 from cultura.api.serializers import LastEventosListSerializer
 from cultura.models import Evento
-
-class EventosPagination(pagination.PageNumberPagination):
-    page_size = 4
-    page_size_query_param = 'page_size'
-    max_page_size = 100
 
 
 ## CRUD PRINCIPAL PARA SOLICITUDES PUBLICAS 
 class EventosViewSet(viewsets.ModelViewSet):
     queryset = Evento.objects.filter(publicado=True).order_by('-fecha')[:5]  
-    pagination_class = EventosPagination 
     serializer_class = LastEventosListSerializer
 
     def get_permissions(self):
