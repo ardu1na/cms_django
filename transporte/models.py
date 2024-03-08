3df263a595d320245d72849280ec05e6c2a7a5ef
ardu1na <arduinadelbosque@gmail.com>
2024-03-08 13:38:22 -0300

    commenting models definition and apps purpose

--- transporte/models.py
+++ transporte/models.py
@@ -1,11 +1,18 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+# Definición de las tablas de precios para la dirección de transporte
+# - los valores en el sitio web se actualizarán dinámicamente
+#   en función de la información cargada en la base de datos
+
+
 class Categoria(models.Model):
     nombre = models.CharField(
                             max_length=300
                             )
-
+    def __str__ (self):
+        return self.nombre
+       
 class Valor(models.Model):
     class Meta:
         verbose_name_plural = "valores"
