f2c6827640c16edd43795beca26a4824f80d8067
ardu1na <arduinadelbosque@gmail.com>
2024-03-08 09:18:39 -0300

    clean app and start modeling

--- transporte/models.py
+++ transporte/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Categoria(models.Model):
+    nombre = models.CharField(max_length=300)
+    
+class Valor(models.Model):
+    class Meta:
+        verbose_name_plural = "valores"
+    
+    categoria = models.ForeignKey(Categoria, on_delete=models.SET_NULL, null=True, blank=True)
+    nombre = models.CharField(max_length=300)
+    precio = models.PositiveIntegerField(default=0)
